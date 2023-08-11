1cf556e4424d6adcfa1dd2bc2b210237633e886a
luk-rs <santos8@gmail.com>
2023-08-11 15:52:14 +0100

    setup proto .cs source generation

--- src/server.rpc/Program.cs
+++ src/server.rpc/Program.cs
@@ -1,3 +1,5 @@
+using server.rpc.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Additional configuration is required to successfully run gRPC on macOS.
@@ -9,7 +11,7 @@ builder.Services.AddGrpc();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-//app.MapGrpcService<GreeterService>();
+app.MapGrpcService<InterviewsService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
---
+++ src/server.rpc/Services/InterviewsService.cs
@@ -0,0 +1,18 @@
+﻿using Grpc.Core;
+
+namespace server.rpc.Services;
+
+public class InterviewsService : Interviews.InterviewsBase
+{
+    private readonly ILogger<InterviewsService> _logger;
+
+    public InterviewsService(ILogger<InterviewsService> logger)
+    {
+        _logger = logger;
+    }
+
+    public override Task<AttemptRegistration> RegisterAttempt(NewAttemptRequest request, ServerCallContext context)
+    {
+        return base.RegisterAttempt(request, context);
+    }
+}
